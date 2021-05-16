ae5157db79913fa61b55d38511a3409ee6496c6c
laurar14 <lorsini.franca@gmail.com>
2021-05-16 17:40:18 +0000

    Added Button component

--- react-traversy-media/react-task-tracker/src/components/Header.js
+++ react-traversy-media/react-task-tracker/src/components/Header.js
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = (e) => {
+        console.log("Click")
+    }
+
     return (
-        <header>
+        <header className="header">
             <h1>{title}</h1>
+            <Button 
+                color="green"
+                text="Add"
+                onClick={onClick}
+            />
         </header>
     )
 }
