ca04cd5c10a5c9f3a37fb5349a42c049dec72f76
laurar14 <lorsini.franca@gmail.com>
2021-05-17 19:28:41 +0000

    Created Task component, installed react-icons, added state and some UI functions

--- react-traversy-media/react-task-tracker/src/App.js
+++ react-traversy-media/react-task-tracker/src/App.js
@@ -1,9 +1,51 @@
-import Header from './components/Header'
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+
+const initialTasks = [
+    {
+        id: 1,
+        text: "Doctor's Appointment",
+        day: "Feb 5th at 2:30pm",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Meeting at School",
+        day: "Feb 6th at 1:30pm",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "Feb 5th at 2:30pm",
+        reminder: false
+    }
+];
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ));
+  }
+
   return (
     <div className="container">
       <Header />
+      { tasks.length ? 
+        (<Tasks 
+          tasks={tasks}
+          onDelete={deleteTask}
+          onToggle={toggleReminder}
+        />) : 
+        (<p>No pending tasks.</p>)
+      }
     </div>
   );
 }
