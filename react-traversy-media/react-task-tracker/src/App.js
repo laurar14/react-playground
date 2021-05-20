dc5951dc8cef588e9f895bed92b26adfd6dadd01
laurar14 <lorsini.franca@gmail.com>
2021-05-20 17:07:16 +0000

    Added mock server with json-server and updated functions to use it

--- react-traversy-media/react-task-tracker/src/App.js
+++ react-traversy-media/react-task-tracker/src/App.js
@@ -1,46 +1,79 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
-
-const initialTasks = [
-    {
-        id: 1,
-        text: "Doctor's Appointment",
-        day: "Feb 5th at 2:30pm",
-        reminder: true
-    },
-    {
-        id: 2,
-        text: "Meeting at School",
-        day: "Feb 6th at 1:30pm",
-        reminder: true
-    },
-    {
-        id: 3,
-        text: "Food Shopping",
-        day: "Feb 5th at 2:30pm",
-        reminder: false
-    }
-];
+// NOTE TO SELF: the command npm run build (or yarn build if using that) will create a build directory. That is the folder we deploy.
+// For local serving, you can install a package called 'serve' installed globally (npm -g i serve) then use the command serve -s build -p 8000 to see the artifact created.
+const API_URL = "http://localhost:5000"
 
 function App() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const deleteTask = (id) => {
+  // Here we are using our mock backend with the package json-server, which created a db.json file for us (check package.json to see the command for 'server')
+  // instead of the data we mocked in our front in the previous version. As you know, useEffect is called on render. Since we're not observing 
+  // any property (or variable), it should run on load, once.
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
+    getTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const res = await fetch(`${API_URL}/tasks`);
+    const data = await res.json();
+    return data;
+  }
+
+  const fetchTask = async (id) => {
+    const res = await fetch(`${API_URL}/tasks/${id}`);
+    const data = await res.json();
+    return data;
+  }
+
+  const deleteTask = async (id) => {
+    await fetch(`${API_URL}/tasks/${id}`, {
+      method: 'DELETE'
+    });
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ));
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+    const res = await fetch(`${API_URL}/tasks/${id}`, {
+      method: 'PUT',
+       headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    });
+
+    const data = await res.json();
+
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task ));
   }
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    // Code for adding new task to the mock data: 
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks([...tasks, newTask]);
+
+    const res = await fetch(`${API_URL}/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json();
+
+    setTasks([...tasks, data]);
   }
 
   return (
